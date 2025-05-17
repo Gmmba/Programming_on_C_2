0a204268eddda0eecdecb3b961d7402e3f22fe5b
Gmmba <tim0fej10065@gmail.com>
2025-05-17 15:41:02 +0700

    Delete RGZ directory

--- RGZ/triangle.h
+++
@@ -1,8 +0,0 @@
-#ifndef TRIANGLE_H
-#define TRIANGLE_H
-
-int triangle_recursive(int n);
-void triangle_to_file(int n, const char *filename);
-int is_valid_positive_integer(const char *str);
-
-#endif
