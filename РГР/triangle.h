354672bbd5e85e9ad8be340d6a26d99b202c5ea4
Gmmba <tim0fej10065@gmail.com>
2025-05-17 15:42:29 +0700

    Add files via upload

---
+++ РГР/triangle.h
@@ -0,0 +1,8 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+int triangle_recursive(int n);
+void triangle_to_file(int n, const char *filename);
+int is_valid_positive_integer(const char *str);
+
+#endif
